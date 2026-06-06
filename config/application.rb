ebf924c92220eb751747609f684bb2059dc83c25
DataIvora <287260320+DataIvora@users.noreply.github.com>
2026-06-06 20:23:45 -0300

    Register Rails database config path

--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,7 @@ Bundler.require(*Rails.groups)
 module TaskRpgRails
   class Application < Rails::Application
     config.load_defaults 8.0
+    config.paths.add "config/database", with: "config/database.yml"
     config.time_zone = "America/Sao_Paulo"
     config.active_record.default_timezone = :local
     config.action_controller.asset_host = ENV["ASSET_HOST"] if ENV["ASSET_HOST"].present?
