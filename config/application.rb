2d68f2b3c5478a50aef77e65c03e723379ee1f41
DataIvora <287260320+DataIvora@users.noreply.github.com>
2026-06-06 20:35:30 -0300

    Use Rails database configuration defaults

--- config/application.rb
+++ config/application.rb
@@ -1,19 +1,12 @@
 require_relative "boot"
 
 require "rails/all"
-require "erb"
-require "yaml"
 
 Bundler.require(*Rails.groups)
 
 module TaskRpgRails
   class Application < Rails::Application
     config.load_defaults 8.0
-    database_config_path = root.join("config/database.yml")
-    config.active_record.database_configuration = YAML.safe_load(
-      ERB.new(database_config_path.read).result,
-      aliases: true
-    )
     config.time_zone = "America/Sao_Paulo"
     config.active_record.default_timezone = :local
     config.action_controller.asset_host = ENV["ASSET_HOST"] if ENV["ASSET_HOST"].present?
--- config/initializers/database_configuration.rb
+++
@@ -1,7 +0,0 @@
-database_config = YAML.safe_load(
-  ERB.new(Rails.root.join("config/database.yml").read).result,
-  aliases: true
-)
-
-ActiveRecord::Base.configurations = database_config
-ActiveRecord::Base.establish_connection(Rails.env.to_sym)
